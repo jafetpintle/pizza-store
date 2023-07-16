a800ed04599050f9c603cc7aa38ee14c9204d746
jafet <jafetpintle@gmail.com>
2023-07-15 22:43:09 -0600

    Pizza service and controller

---
+++ src/main/java/com/japy/pizzashop/service/PizzaService.java
@@ -0,0 +1,23 @@
+package com.japy.pizzashop.service;
+
+import com.japy.pizzashop.persitence.entity.PizzaEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PizzaService {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PizzaService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<PizzaEntity> getAll(){
+        return this.jdbcTemplate.query("SELECT * FROM PIZZA", new BeanPropertyRowMapper<>(PizzaEntity.class));
+    }
+}
---
+++ src/main/java/com/japy/pizzashop/web/controller/PizzaController.java
@@ -0,0 +1,27 @@
+package com.japy.pizzashop.web.controller;
+
+import com.japy.pizzashop.persitence.entity.PizzaEntity;
+import com.japy.pizzashop.service.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pizzas")
+public class PizzaController {
+    private final PizzaService pizzaService;
+
+    @Autowired
+    public PizzaController(PizzaService pizzaService) {
+        this.pizzaService = pizzaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PizzaEntity>> getAll(){
+        return ResponseEntity.ok(this.pizzaService.getAll());
+    }
+}
